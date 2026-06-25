bcc61227f7e67390ed942e16bbdfb9acebef628b
Dave SiuuCode <david.armijo101@gmail.com>
2026-06-25 16:11:43 -0500

    startup script - Changed from browser choice from chrome to zen

--- .config/scripts/startup-scripts/startup.sh
+++ .config/scripts/startup-scripts/startup.sh
@@ -5,11 +5,11 @@ PROFILE=$(rofi -dmenu -p " " \
   <<<$'1 Work Obsidian\n2 Work Xournal\n3 Gaming')
 
 # Load browser env configs
-if [ -r "$HOME/.config/startup-config/chrome-env.env" ]; then
+if [ -r "$HOME/.config/startup-config/browser-env.env" ]; then
   # Avoid leaking secrets if xtrace is on
   case $- in *x*) set +x ;; esac
   set -a # auto-export all vars defined while set
-  . "$HOME/.config/startup-config/chrome-env.env"
+  . "$HOME/.config/startup-config/browser-env.env"
   set +a
 fi
 
@@ -18,25 +18,29 @@ case "$PROFILE" in
 "1 Work Obsidian")
   hyprctl dispatch exec [workspace 1 silent] discord
   hyprctl dispatch exec [workspace 1 silent] spotify
-  hyprctl dispatch exec [workspace 2 silent] "google-chrome-stable --profile-directory="Default" file:///home/Arieldynamic/.dotfiles/.config/scripts/startup-scripts/startup-chrome-supplements/default-tab.html"
+  hyprctl dispatch exec [workspace 2 silent] "zen-browser -P \"${PERSONAL_PROFILE_NAME}\" file://${HOME}/.dotfiles/.config/scripts/startup-scripts/startup-chrome-supplements/default-tab.html"
   hyprctl dispatch exec [workspace 3 silent] "ghostty --title="Terminal" -e bash"
-  hyprctl dispatch exec [workspace special:magic silent] "google-chrome-stable --profile-directory="${WORK_ACCOUNT_NAME}" file:///home/Arieldynamic/.dotfiles/.config/scripts/startup-scripts/startup-chrome-supplements/work-tab.html"
+  hyprctl dispatch exec [workspace special:magic silent] "zen-browser -P \"${WORK_PROFILE_NAME}\" file://${HOME}/.dotfiles/.config/scripts/startup-scripts/startup-chrome-supplements/work-tab.html"
   hyprctl dispatch exec [workspace 4 silent] obsidian
+  # hyprctl dispatch exec [workspace 2 silent] "google-chrome-stable --profile-directory="Default" file://${HOME}/.dotfiles/.config/scripts/startup-scripts/startup-chrome-supplements/default-tab.html"
+  # hyprctl dispatch exec [workspace special:magic silent] "google-chrome-stable --profile-directory="${WORK_ACCOUNT_NAME}" file://${HOME}/.dotfiles/.config/scripts/startup-scripts/startup-chrome-supplements/work-tab.html"
   ;;
 "2 Work Xournal")
   hyprctl dispatch exec [workspace 1 silent] discord
   hyprctl dispatch exec [workspace 1 silent] spotify
-  hyprctl dispatch exec [workspace 2 silent] "google-chrome-stable --profile-directory="Default" file:///home/Arieldynamic/.dotfiles/.config/scripts/startup-scripts/startup-chrome-supplements/default-tab.html"
+  hyprctl dispatch exec [workspace 2 silent] "zen-browser -P \"${PERSONAL_PROFILE_NAME}\" file://${HOME}/.dotfiles/.config/scripts/startup-scripts/startup-chrome-supplements/default-tab.html"
   hyprctl dispatch exec [workspace 3 silent] "ghostty --title="Terminal" -e bash"
-  hyprctl dispatch exec [workspace special:magic silent] "google-chrome-stable --profile-directory="${WORK_ACCOUNT_NAME}" file:///home/Arieldynamic/.dotfiles/.config/scripts/startup-scripts/startup-chrome-supplements/work-tab.html"
+  hyprctl dispatch exec [workspace special:magic silent] "zen-browser -P \"${WORK_PROFILE_NAME}\" file://${HOME}/.dotfiles/.config/scripts/startup-scripts/startup-chrome-supplements/work-tab.html"
   hyprctl dispatch exec [workspace 4 silent] xournalpp
+  # hyprctl dispatch exec [workspace 2 silent] "google-chrome-stable --profile-directory="Default" file://${HOME}/.dotfiles/.config/scripts/startup-scripts/startup-chrome-supplements/default-tab.html"
+  # hyprctl dispatch exec [workspace special:magic silent] "google-chrome-stable --profile-directory="${WORK_ACCOUNT_NAME}" file://${HOME}/.dotfiles/.config/scripts/startup-scripts/startup-chrome-supplements/work-tab.html"
   ;;
 "3 Gaming")
   hyprctl dispatch exec [workspace 1 silent] discord
   hyprctl dispatch exec [workspace 1 silent] spotify
-  hyprctl dispatch exec [workspace 2 silent] "google-chrome-stable --profile-directory="Default" file:///home/Arieldynamic/.dotfiles/.config/scripts/startup-scripts/startup-chrome-supplements/default-tab.html"
-  hyprctl dispatch exec [workspace 3 silent] "ghostty --title="Terminal" -e bash"
+  hyprctl dispatch exec [workspace 2 silent] "zen-browser -P \"${PERSONAL_PROFILE_NAME}\" file://${HOME}/.dotfiles/.config/scripts/startup-scripts/startup-chrome-supplements/default-tab.html" hyprctl dispatch exec [workspace 3 silent] "ghostty --title="Terminal" -e bash"
   hyprctl dispatch exec [workspace 4 silent] steam
+  # hyprctl dispatch exec [workspace 2 silent] "google-chrome-stable --profile-directory="Default" file://${HOME}/.dotfiles/.config/scripts/startup-scripts/startup-chrome-supplements/default-tab.html"
   ;;
 *) ;;
 esac
